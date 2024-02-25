313de3e7c4d01cc9dd8965adc40c0bd5c0155ca9
Maxime <sowlution.pro@gmail.com>
2024-02-24 18:51:17 -0700

    add routing system

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,29 @@
-
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/HomePage'; 
 import Navbar from './components/Navbar';
 import Footer from './components/Footer'; 
-import logo from './assets/cypher_zero_logo.png'
+import logo from './assets/cypher_zero_logo.png';
+import TransactionDetailsPage from './pages/TransactionDetailsPage'; // Assurez-vous que ce composant existe
+import BlockDetailsPage from './pages/BlockDetailsPage';
+import NotFound from './pages/NotFound';
+
 function App() {
   return (
-    <div className="App">
-      <Navbar logo={logo}/>
-      <div className="App-content">
-        <HomePage />
+    <Router>
+      <div className="App">
+        <Navbar logo={logo}/>
+        <div className="App-content">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/transaction/:hash" element={<TransactionDetailsPage />} />
+            <Route path="/block/:hash" element={<BlockDetailsPage />} />
+            <Route path="*" element={<NotFound/>}/>
+          </Routes>
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
+    </Router>
   );
 }
 
